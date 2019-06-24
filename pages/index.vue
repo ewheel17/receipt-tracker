0707307267c0589a6602e69ae26c086404bd4310
elliott max wheeler <ewheel17@gmail.com>
2019-06-23 20:38:29 -0600

    setup

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-xs-center">
+        <!-- <logo />
+        <vuetify-logo /> -->
+      </div>
+      <v-card>
+        <v-card-title class="headline"
+          >Welcome to the receipt-reader app.</v-card-title
+        >
+        <v-card-text></v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" flat nuxt to="/receipts"
+            >View Receipts</v-btn
+          >
+          <v-spacer />
+          <v-btn color="primary" flat nuxt to="/add-receipt">Add Receipt</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {}
+</script>
